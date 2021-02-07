3a090d30130b7e82fe8e82ebcb281e51cfe87794
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-02-07 22:10:51 +0100

    cleaned code

--- public/js/projectApp.js
+++ public/js/projectApp.js
@@ -1,23 +1,17 @@
-window.onload = () => {
-
-    const contactColumn = document.getElementById('footer-txt-column');
-    const contactRow = document.getElementById('footer-txt-row');
-
-    function smallscreenView() {
-        contactColumn.classList.remove('display-none');
-        contactRow.classList.add('display-none');
-    }
-
-    function bigscreenView() {
-        contactColumn.classList.add('display-none');
-        contactRow.classList.remove('display-none');
-    }
-
-    window.addEventListener('resize', () => {
-        if (window.innerWidth < 970) {
-            smallscreenView()
-        } else if (window.innerWidth > 970) {
-            bigscreenView()
-        }
-    })
+function smallscreenView() {
+    contactColumn.classList.remove('display-none');
+    contactRow.classList.add('display-none');
 }
+
+function bigscreenView() {
+    contactColumn.classList.add('display-none');
+    contactRow.classList.remove('display-none');
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 970) {
+        smallscreenView()
+    } else if (window.innerWidth > 970) {
+        bigscreenView()
+    }
+})
