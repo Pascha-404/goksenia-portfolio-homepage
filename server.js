ec4738a76e6ed1cf2a66e2896d9c2537f8f20744
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-02-07 20:26:34 +0100

    added page and database logic for projects

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const {
+    request
+} = require('http');
 const app = express();
 const port = 8080;
 const path = require('path')
+const projectData = require('./projects.json')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -16,6 +20,17 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('*', (req, res) => {
-    res.send('<h1>Page not found - 404</h1>')
+app.get('/project/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const project = projectData.find(d => d.id === id);
+    res.render('projects', {
+        project
+    })
+
 })
+
+// app.get('*', (req, res) => {
+//     res.send('<h1>Page not found - 404</h1>')
+// })
