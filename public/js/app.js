4dcc711446001e8d342c453aceb5ac33510f18b6
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-02-07 22:35:02 +0100

    fixed view function

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,32 @@ const contactRow = document.querySelector('.contact-row')
 
 
 // activates the right classes for small and big screens
-window.addEventListener('resize', () => {
+function smallViewFooter() {
+    contactColumn.classList.remove('display-none');
+    contactRow.classList.add('display-none');
+}
+
+function bigViewFooter() {
+    contactColumn.classList.add('display-none');
+    contactRow.classList.remove('display-none');
+}
+
+function adjustViewFooter() {
     if (window.innerWidth < 970) {
-        smallscreenView()
+        smallViewFooter()
     } else if (window.innerWidth > 970) {
-        bigscreenView()
+        bigViewFooter()
     }
-})
+}
+
+adjustViewFooter()
+
+window.addEventListener('resize', adjustViewFooter())
+
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth < 970) {
+//         smallscreenView()
+//     } else if (window.innerWidth > 970) {
+//         bigscreenView()
+//     }
+// })
--- public/js/homeApp.js
+++ public/js/homeApp.js
@@ -5,28 +5,36 @@ const scrollBtn = document.querySelector('#scroll');
 const scrollTxt = document.querySelector('#scroll-txt');
 
 // Toggles hiding for figures at small screens (<970px width)
-function smallscreenView() {
+function smallViewHome() {
     figRow.forEach(figRow => {
         figRow.classList.add('display-none');
     })
     figColumn.forEach(figColumn => {
         figColumn.classList.remove('display-none');
     })
-    contactColumn.classList.remove('display-none');
-    contactRow.classList.add('display-none');
 }
 
 // Sets everything back for big screen view (>970px width)
-function bigscreenView() {
+function bigViewHome() {
     figRow.forEach(figRow => {
         figRow.classList.remove('display-none')
     })
     figColumn.forEach(figColumn => {
         figColumn.classList.add('display-none')
     })
-    contactColumn.classList.add('display-none');
-    contactRow.classList.remove('display-none');
 }
+
+function adjustViewHome() {
+    if (window.innerWidth < 970) {
+        smallViewHome()
+    } else if (window.innerWidth > 970) {
+        bigViewHome()
+    }
+}
+
+adjustViewHome()
+
+window.addEventListener('resize', adjustViewHome)
 
 // let scrollPos = 0;
 // window.addEventListener('scroll', () => {
