a44ec93577d40a8d5381fcd89446e269e3538a40
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-02-13 10:52:31 +0100

    +animation menu and clickevent on contactBtn

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const contactColumn = document.querySelector('.contact-column');
 const contactRow = document.querySelector('.contact-row');
 const menuBtn = document.querySelector('#menu-btn');
 const menuSmall = document.querySelector('#menu-small');
+const contactBtn = document.querySelectorAll('.contact-btn')
 
 
 
@@ -24,10 +25,21 @@ function adjustViewFooter() {
     }
 }
 
+// adjusts all classes correct, ones
 adjustViewFooter()
 
+//  eventListener for correct classes on resizing
 window.addEventListener('resize', adjustViewFooter())
 
+// adds in smallMenu and animation
 menuBtn.addEventListener('click', () => {
-    menuSmall.classList.toggle('active')
+    menuSmall.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+})
+
+// ClickEvent for each contactBtn -> to footer
+contactBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "#contact"
+    })
 })
