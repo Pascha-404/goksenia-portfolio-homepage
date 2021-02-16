5d74d103a069947a230c1f06b05c5e1324ba20a7
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-02-16 21:42:39 +0100

    +animation on scroll for figures

--- public/js/homeApp.js
+++ public/js/homeApp.js
@@ -1,8 +1,10 @@
 const heroBtn = document.querySelector('#heroBtn');
 const figRow = document.querySelectorAll('.figure-row');
 const figColumn = document.querySelectorAll('.figure-column');
-const scrollBtn = document.querySelector('#scroll');
-const scrollTxt = document.querySelector('#scroll-txt');
+
+const scroll = document.querySelector('#scroll');
+const bodyHeight = document.body.offsetHeight;
+const windowHeight = window.innerHeight;
 
 // Toggles hiding for figures at small screens (<970px width)
 function smallViewHome() {
@@ -36,17 +38,84 @@ adjustViewHome()
 
 window.addEventListener('resize', adjustViewHome)
 
-// let scrollPos = 0;
-// window.addEventListener('scroll', () => {
-//     if ((document.body.getBoundingClientRect()).top > scrollPos) {
-//         scrollBtn.classList.add('active')
-//     } else {
-//         scrollBtn.classList.remove('active')
-//     }
-//     scrollPos = (document.body.getBoundingClientRect()).top;
-// })
 
-// send user to portfolio section
-// heroBtn.addEventListener('click', () => {
-//     window.location.href = '#portfolio';
-// })
+// hides the scroll-box at reaching bottom of page
+function scrollHide() {
+    let scrollpos = window.scrollY;
+    if (bodyHeight <= (scrollpos + windowHeight)) {
+        scroll.classList.add('active');
+    }
+}
+
+window.addEventListener('wheel', (event) => {
+    scrollHide()
+});
+
+
+// click on figure to project-page + removing active class
+
+const figGroupChat = document.querySelectorAll('.fig-groupChat');
+const figTeamWeb = document.querySelectorAll('.fig-teamWeb');
+const figWineryWeb = document.querySelectorAll('.fig-wineryWeb');
+const figMoodBooster = document.querySelectorAll('.fig-moodBooster');
+
+const delay = 200;
+
+function locationChange(link) {
+    location.href = `${link}`
+}
+
+function goToProject(link) {
+    setTimeout(() => {
+        locationChange(link)
+    }, delay)
+}
+
+figGroupChat.forEach(figure => {
+    figure.addEventListener('click', () => {
+        figure.classList.remove('active');
+        goToProject('/project/chatApp');
+    })
+})
+figTeamWeb.forEach(figure => {
+    figure.addEventListener('click', () => {
+        figure.classList.remove('active');
+        goToProject('/project/teamWeb');
+    })
+})
+figWineryWeb.forEach(figure => {
+    figure.addEventListener('click', () => {
+        figure.classList.remove('active');
+        goToProject('/project/wineryWeb');
+    })
+})
+figMoodBooster.forEach(figure => {
+    figure.addEventListener('click', () => {
+        figure.classList.remove('active');
+        goToProject('/project/moodBoosterApp');
+    })
+})
+
+// transforms figures in and out depending on location at page
+
+const projectFigures = document.querySelectorAll('.fig-projects');
+
+function checkFigures() {
+    const triggerHeight = windowHeight * 0.9;
+
+    projectFigures.forEach(figure => {
+        const figureTop = figure.getBoundingClientRect().top;
+
+        if (triggerHeight > figureTop) {
+            figure.classList.add('active');
+        } else {
+            figure.classList.remove('active')
+        }
+    })
+}
+
+window.addEventListener('scroll', () => {
+    checkFigures()
+})
+
+checkFigures()
