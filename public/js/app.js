862174c0b4976f2e901dc5fff082f01901dd5c7e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-02-18 21:11:55 +0100

    +logic for footer buttons

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ const contactRow = document.querySelector('.contact-row');
 const menuBtn = document.querySelector('#menu-btn');
 const menuSmall = document.querySelector('#menu-small');
 const contactBtn = document.querySelectorAll('.contact-btn');
+const dataColumn = document.querySelector('.data-column');
+const dataRow = document.querySelector('.data-row');
 
 
 
@@ -12,11 +14,15 @@ const logo = document.querySelector('#logo')
 function smallViewFooter() {
     contactColumn.classList.remove('display-none');
     contactRow.classList.add('display-none');
+    dataRow.classList.add('display-none');
+    dataColumn.classList.remove('display-none');
 }
 
 function bigViewFooter() {
     contactColumn.classList.add('display-none');
     contactRow.classList.remove('display-none');
+    dataRow.classList.remove('display-none');
+    dataColumn.classList.add('display-none');
 }
 
 function adjustViewFooter() {
@@ -50,4 +56,43 @@ contactBtn.forEach(btn => {
 
 logo.addEventListener('click', () => {
     location.href = "/";
+})
+
+
+const aboutWebBtn = document.querySelectorAll('.about-this-website');
+const privacyPolBtn = document.querySelectorAll('.privacy-policy');
+const privacyPolBox = document.querySelector('#privacy-policy-box');
+const aboutWebBox = document.querySelector('#about-website');
+const closeBox = document.querySelectorAll('.closeBox');
+const shadowBgBox = document.querySelector('#shadow-bg-box');
+
+aboutWebBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        aboutWebBox.classList.toggle('active');
+        shadowBgBox.classList.toggle('active');
+    })
+})
+
+privacyPolBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        privacyPolBox.classList.toggle('active');
+        shadowBgBox.classList.toggle('active');
+    })
+})
+
+function hideAllBoxes() {
+    privacyPolBox.classList.remove('active');
+    aboutWebBox.classList.remove('active');
+    shadowBgBox.classList.remove('active');
+}
+
+shadowBgBox.addEventListener('click', () => {
+    hideAllBoxes()
+})
+
+closeBox.forEach(btn => {
+    btn.addEventListener('click', () => {
+        hideAllBoxes()
+        console.log('worked')
+    })
 })
