7212c0fff34717c66692edb54c5c0e55c632f763
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-03-08 17:00:39 +0100

    added js file + click events

---
+++ public/js/cmsApp.js
@@ -0,0 +1,30 @@
+const btns = document.querySelectorAll('.btn');
+const existingProjects = document.querySelectorAll('.existing-project');
+const addProjectBtn = document.querySelector('#addProject-btn')
+const newProject = document.querySelector('.new');
+const transitionBox = document.querySelector('.transition-box');
+const contentWrap = document.querySelector('.content-wrap')
+const navbar = document.querySelector('.navbar')
+
+function pageTransition() {
+    transitionBox.classList.toggle('active');
+    contentWrap.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('mouseenter', () => {
+        btn.classList.add('mouseIn')
+    })
+})
+
+btns.forEach(btn => {
+    btn.addEventListener('mouseleave', () => {
+        btn.classList.remove('mouseIn')
+    })
+})
+
+addProjectBtn.addEventListener('click', () => {
+    pageTransition()
+
+})
