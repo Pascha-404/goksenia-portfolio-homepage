35163fd58b7c3ee0e1c2c14cb0f75b6f23977adf
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-03-29 23:14:36 +0200

    seperate file for public routes

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,2 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utilitys/catchAsync')
+const Project = require('../models/project');
+
+// route for mainpage
+router.get('/', catchAsync(async (req, res) => {
+    // searches for projects with firstProject tag = true
+    const firstProjects = await Project.find({
+        'tags.firstProject': true,
+        'tags.hideProject': {
+            $ne: true
+        }
+    });
+
+    const secondProjects = await Project.find({
+        'tags.secondProject': true,
+        'tags.hideProject': {
+            $ne: true
+        }
+    });
+
+    // searches for projects with latestWork tag = true and not already in firstProjects
+    const latestProjects = await Project.find({
+        'tags.latestWork': true,
+        'tags.firstProject': {
+            $ne: true
+        },
+        'tags.secondProject': {
+            $ne: true
+        },
+        'tags.hideProject': {
+            $ne: true
+        }
+    })
+
+    // searches all other projects
+    const projects = await Project.find({
+        'tags.firstProject': {
+            $ne: true
+        },
+        'tags.secondProject': {
+            $ne: true
+        },
+        'tags.latestWork': {
+            $ne: true
+        },
+        'tags.hideProject': {
+            $ne: true
+        }
+    })
+
+    res.render('index', {
+        firstProjects,
+        secondProjects,
+        latestProjects,
+        projects
+    })
+}));
+
+// route for single projectpage
+router.get('/project/:urlName', catchAsync(async (req, res) => {
+    const {
+        urlName
+    } = req.params;
+    const project = await Project.findOne({
+        urlName: urlName
+    });
+    if (!project) throw new ExpressError('Project Not Found', 404);
+    res.render('projects', {
+        project
+    });
+
+}));
+
+module.exports = router;
