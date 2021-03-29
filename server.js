5bbb43792fd331ce1bd37daa0bca5681b64bc441
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-03-29 23:14:11 +0200

    cleaned code, routes in seperate file

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const path = require('path')
 const methodOverride = require('method-override');
-
-const catchAsync = require('./utilitys/catchAsync');
 const ExpressError = require('./utilitys/expressError');
-const Project = require('./models/project');
 const mongoose = require('mongoose');
 const cmsRoutes = require('./routes/cmsRoutes');
-const validateProject = require('./utilitys/validateProject')
+const publicRoutes = require('./routes/publicRoutes')
 
 // connects database with app //
 mongoose.connect('mongodb://localhost/goksenia', {
@@ -31,90 +27,20 @@ app.use(express.urlencoded({
 }));
 
 app.use(methodOverride('_method'));
-
 app.use(express.static('public'));
 
-app.use('/projects', cmsRoutes)
+// routing for cms
+app.use('/cms', cmsRoutes);
 
-// route for mainpage
-app.get('/', catchAsync(async (req, res) => {
-    // searches for projects with firstProject tag = true
-    const firstProjects = await Project.find({
-        'tags.firstProject': true,
-        'tags.hideProject': {
-            $ne: true
-        }
-    });
-
-    const secondProjects = await Project.find({
-        'tags.secondProject': true,
-        'tags.hideProject': {
-            $ne: true
-        }
-    });
-
-    // searches for projects with latestWork tag = true and not already in firstProjects
-    const latestProjects = await Project.find({
-        'tags.latestWork': true,
-        'tags.firstProject': {
-            $ne: true
-        },
-        'tags.secondProject': {
-            $ne: true
-        },
-        'tags.hideProject': {
-            $ne: true
-        }
-    })
-
-    // searches all other projects
-    const projects = await Project.find({
-        'tags.firstProject': {
-            $ne: true
-        },
-        'tags.secondProject': {
-            $ne: true
-        },
-        'tags.latestWork': {
-            $ne: true
-        },
-        'tags.hideProject': {
-            $ne: true
-        }
-    })
-
-    res.render('index', {
-        firstProjects,
-        secondProjects,
-        latestProjects,
-        projects
-    })
-}));
-
-// route for single projectpage
-app.get('/project/:urlName', catchAsync(async (req, res) => {
-    const {
-        urlName
-    } = req.params;
-    const project = await Project.findOne({
-        urlName: urlName
-    });
-    if (!project) throw new ExpressError('Project Not Found', 404);
-    res.render('projects', {
-        project
-    });
-
-}));
-
-
-
-
+// routing for public pages
+app.use('/', publicRoutes);
 
 // 404 route
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 });
 
+// error handler
 app.use((err, req, res, next) => {
     const {
         statusCode = 500
