9e5b06b595050ccf2c32f0b04255164a6dfe6a6b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-04-03 23:53:35 +0200

    cleaned code

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const catchAsync = require('../utilitys/catchAsync')
+
 const Project = require('../models/project');
 
 // route for mainpage
