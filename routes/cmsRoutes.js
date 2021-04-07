64344dc9aa4798a11e35450699d85cdb04afb893
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-04-07 20:57:47 +0200

    added middleware for protecting cms routes

--- routes/cmsRoutes.js
+++ routes/cmsRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const catchAsync = require('../utilitys/catchAsync')
 const validateProject = require('../utilitys/validateProject')
+const isLoggedIn = require('../utilitys/isLoggedIn')
 
 const Project = require('../models/project');
 
 
 // route for cms index/dashboard page
-router.get('/index', catchAsync(async (req, res) => {
+router.get('/index', isLoggedIn, catchAsync(async (req, res) => {
     const projects = await Project.find({});
     res.render('./cms/projectsIndex', {
         projects
@@ -16,12 +17,12 @@ router.get('/index', catchAsync(async (req, res) => {
 }));
 
 // route for add-page of new projects
-router.get('/add', (req, res) => {
+router.get('/add', isLoggedIn, (req, res) => {
     res.render('./cms/projectsAdd');
 })
 
 // route for editing existing projects
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const {
         id
     } = req.params;
@@ -36,7 +37,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 }));
 
 // update route for projects
-router.put('/:id', validateProject, catchAsync(async (req, res) => {
+router.put('/:id', validateProject, isLoggedIn, catchAsync(async (req, res) => {
     const {
         id
     } = req.params;
@@ -50,7 +51,7 @@ router.put('/:id', validateProject, catchAsync(async (req, res) => {
 }))
 
 // delete route for projects
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const {
         id
     } = req.params;
@@ -60,7 +61,7 @@ router.delete('/:id', catchAsync(async (req, res) => {
 }));
 
 // route for posting new project
-router.post('/add', validateProject, catchAsync(async (req, res) => {
+router.post('/add', validateProject, isLoggedIn, catchAsync(async (req, res) => {
     const project = new Project(req.body);
     await project.save();
     req.flash('success', `Succesfully created "${project.title}"!`)
