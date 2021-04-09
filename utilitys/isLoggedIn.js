2561afb1a34adfcbb0bc8e7df02176cd07889c6a
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-04-09 20:37:20 +0200

    takes orig.Url and saves to session

--- utilitys/isLoggedIn.js
+++ utilitys/isLoggedIn.js
@@ -1,5 +1,6 @@
 const isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'Sign in first!');
         return res.redirect('/login');
     }
