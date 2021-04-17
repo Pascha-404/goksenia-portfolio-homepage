67a5bbe12f440d73b2f98e43e31c8ffea5db90d1
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-04-17 22:57:39 +0200

    first test with mulder and cloudinary

--- routes/cmsRoutes.js
+++ routes/cmsRoutes.js
@@ -1,21 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
-const catchAsync = require('../utilitys/catchAsync')
+const catchAsync = require('../utilitys/catchAsync');
 const {
     isLoggedIn,
     hasPermission,
     validateProject
-} = require('../middleware')
+} = require('../middleware');
 
-const cms = require('../controllers/cms')
+const cms = require('../controllers/cms');
+
+const {
+    storage
+} = require('../cloudinary')
+const multer = require('multer');
+const upload = multer({
+    storage
+});
+const uploadInput = upload.fields([{
+    name: 'imgHome'
+}, {
+    name: 'imgProject1'
+}, {
+    name: 'imgProject2'
+}]);
+
 
 // route for cms index/dashboard page
 router.get('/index', isLoggedIn, catchAsync(cms.index));
 
 router.route('/add')
     .get(isLoggedIn, cms.showAddPage)
-    .post(validateProject, isLoggedIn, hasPermission, catchAsync(cms.addPage))
+    // .post(validateProject, isLoggedIn, hasPermission, catchAsync(cms.addPage))
+    .post(uploadInput, (req, res) => {
+        if (req.files.imgHome) {
+            const filename1 = req.files.imgHome[0].originalname;
+            console.log(filename1)
+        }
+        if (req.files.imgProject1) {
+            const filename2 = req.files.imgProject1[0].originalname;
+            console.log(filename2)
+        }
+        if (req.files.imgProject2) {
+            const filename3 = req.files.imgProject2[0].originalname;
+            console.log(filename3)
+        }
+        res.send('Finished upload!')
+    })
 
 // route for editing existing projects
 router.get('/:id/edit', isLoggedIn, catchAsync(cms.showEditProject));
