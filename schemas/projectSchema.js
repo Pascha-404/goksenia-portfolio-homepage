e234cb39102c50ba0320a61a704b330718221398
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-04-29 17:29:42 +0200

    added htmlSanitizer for projectValidation

--- schemas/projectSchema.js
+++ schemas/projectSchema.js
@@ -1,16 +1,56 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not contain any html tags.',
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {}
+                });
+                if (clean !== value) {
+                    return helpers.error('string.escapeHTML', {
+                        value
+                    })
+                } else {
+                    return clean;
+                }
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.projectSchema = Joi.object({
     urlName: Joi.string()
         .trim()
         .min(3)
-        .required(),
+        .required()
+        .escapeHTML(),
 
-    title: Joi.string().trim().min(5).required(),
+    title: Joi.string()
+        .trim()
+        .min(5)
+        .required()
+        .escapeHTML(),
 
     description: {
-        homeTxt: Joi.string().trim().required(),
-        projectTxt: Joi.string().trim().required()
+        homeTxt: Joi.string()
+            .trim()
+            .required()
+            .escapeHTML(),
+
+        projectTxt: Joi.string()
+            .trim()
+            .required()
+            .escapeHTML()
     },
 
     tags: {
@@ -27,20 +67,35 @@ module.exports.projectSchema = Joi.object({
 
     images: {
         imgHome: {
-            url: Joi.string().allow(''),
-            filename: Joi.string().allow('')
+            url: Joi.string()
+                .allow('')
+                .escapeHTML(),
+
+            filename: Joi.string()
+                .allow('')
+                .escapeHTML()
         },
         imgProject1: {
-            url: Joi.string().allow(''),
-            filename: Joi.string().allow('')
+            url: Joi.string()
+                .allow('')
+                .escapeHTML(),
+
+            filename: Joi.string()
+                .allow('')
+                .escapeHTML()
         },
         imgProject2: {
-            url: Joi.string().allow(''),
-            filename: Joi.string().allow('')
+            url: Joi.string()
+                .allow('')
+                .escapeHTML(),
+
+            filename: Joi.string()
+                .allow('')
+                .escapeHTML()
         }
     },
 
     deleteImg: Joi.array(),
 
-    previewLink: Joi.string().allow('')
+    previewLink: Joi.string().allow('').escapeHTML()
 })
