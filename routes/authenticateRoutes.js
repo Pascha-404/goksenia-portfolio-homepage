df75cdb59fe05ffb48e15570d3ab6d31e267913e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-02 15:56:57 +0200

    fixed semicolon

--- routes/authenticateRoutes.js
+++ routes/authenticateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport')
+const passport = require('passport');
 
 const catchAsync = require('../utilitys/catchAsync');
 const {
